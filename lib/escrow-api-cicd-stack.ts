51408784781e33a297dc67c4f51dd72f8ca2c752
Big Ralph <mr.uwaifo@gmail.com>
2023-12-01 07:44:10 +0100

    first

--- lib/escrow-api-cicd-stack.ts
+++ lib/escrow-api-cicd-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EscrowApiCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EscrowApiCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "EscrowPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("mohamed-elgamal/escrow-api", "main"),
+        commands: ["npm ci", "npm run build", "npx synth"],
+      }),
+    });
   }
 }
