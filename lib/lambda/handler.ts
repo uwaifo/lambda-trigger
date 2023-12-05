93c2246b96568511cd08dd76229f37f58dd7e5d6
Big Ralph <mr.uwaifo@gmail.com>
2023-12-05 14:24:43 +0100

    update

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,7 +1,7 @@
 export async function handler(event:string, context: string) {
     console.log('Statge Name is :'+ process.env.stage);
     return {
-        body : 'Hello from Escrow Lmabda Function',
+        body : 'Hello from Escrow Lambda Function',
         statusCode: 200,
     }
     
